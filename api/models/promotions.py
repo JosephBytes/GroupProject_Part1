c6b2cf36c1a1ad0605d570c5a563f77834c794fe
Kiyah <nrollin4@charlotte.edu>
2024-07-26 17:45:44 -0400

    feat: added primary key for promotion model

--- api/models/promotions.py
+++ api/models/promotions.py
@@ -7,5 +7,6 @@ from ..dependencies.database import Base
 class Promotions(Base):
     __tablename__ = "promotions"
 
+    promotion_id = Column(Integer, primary_key=True, autoincrement=True)
     code = Column(Integer, primary_key=True, index=True, autoincrement=True)
     expiration_date = Column(DATETIME)
