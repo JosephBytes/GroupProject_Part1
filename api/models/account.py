4db4aa310def22c89bec30726fed259523027eb5
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-07-26 16:46:59 -0400

    feat: refactored variable names in account.py

--- api/models/account.py
+++ api/models/account.py
@@ -8,7 +8,7 @@ class Account(Base):
     __tablename__ = "account"
 
     account_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-
+    name = Column(String(100))
     email = Column(String(100))
     phone = Column(Integer)
     address = Column(String(100))
