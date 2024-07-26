482335c652da303e8d5b4ffb112ba9df1d3550d5
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-07-26 17:29:43 -0400

    feat: refactored variable names in recipes.py

--- api/models/recipes.py
+++ api/models/recipes.py
@@ -8,9 +8,9 @@ class Recipe(Base):
     __tablename__ = "recipes"
 
     recipe_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    item_id = Column(Integer, ForeignKey("items.id"))
+    dish_id = Column(Integer, ForeignKey("items.dish_id"))
     resource_id = Column(Integer, ForeignKey("resources.id"))
-    amount = Column(Integer, index=True, nullable=False, server_default='0.0')
+    cost = Column(Integer, index=True, nullable=False, server_default='0.0')
 
     menu_item = relationship("Items", back_populates="recipes")
     resources = relationship("Resource", back_populates="recipes")
