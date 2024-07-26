bf3537b30e38afb50730c7f2876cbfe1d736761b
kspark12 <kspark12@uncc.edu>
2024-07-26 17:20:17 -0400

    feat: comment on account.py

--- api/schemas/account.py
+++ api/schemas/account.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 from pydantic import BaseModel, EmailStr
 #account should be here
+#account is literally right here
 class AccountBase(BaseModel):
     email: EmailStr
     phone: int
