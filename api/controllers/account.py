72748bbc1e7a621ca3b936613b1bf4770444cf30
Kiyah <nrollin4@charlotte.edu>
2024-07-26 16:25:37 -0400

    feat: added CRUD operations for account controller

--- api/controllers/account.py
+++ api/controllers/account.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, status, Response, Depends
 from ..models import account as model
 from sqlalchemy.exc import SQLAlchemyError
 
-#hello
+
 
 def create(db: Session, request):
     new_item = model.Account(
