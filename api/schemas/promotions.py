9eb75a59351d855ddb1915d84ab4ab1463de9c56
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-07-26 20:31:01 -0400

    feat: refactored schemas\promotions.py variable names

--- api/schemas/promotions.py
+++ api/schemas/promotions.py
@@ -2,22 +2,24 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
 
-#promotions
+
 class PromotionsBase(BaseModel):
     expiration_date: datetime
+
 
 class PromotionsCreate(PromotionsBase):
     promotion_id: int
     code: int
+
 
 class PromotionsUpdate(BaseModel):
     promotion_id: Optional[int] = None
     code: Optional[int] = None
     expiration_date: Optional[datetime] = None
 
+
 class Promotions(PromotionsBase):
     promotion_id: int
 
-
-    class Config:
-        orm_mode = True
+    class ConfigDict:
+        from_attributes = True
