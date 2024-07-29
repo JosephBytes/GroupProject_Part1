17bf8ffcad945fcf38cdc806a8ae4278fc213529
Kiyah <nrollin4@charlotte.edu>
2024-07-29 11:33:15 -0400

    fix: corrected classes names

--- api/schemas/menu_item.py
+++ api/schemas/menu_item.py
@@ -2,24 +2,24 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
 
-class MenuItemBase(BaseModel):
+class ItemBase(BaseModel):
     dish: str
     ingredients: str
     price: float
     calories: int
     category: str
 
-class MenuItemCreate(MenuItemBase):
+class ItemCreate(ItemBase):
     pass
 
-class MenuItemUpdate(BaseModel):
+class ItemUpdate(BaseModel):
     dish: Optional[str] = None
     ingredients: Optional[str] = None
     price: Optional[float] = None
     calories: Optional[int] = None
     category: Optional[str] = None
 
-class MenuItem(MenuItemBase):
+class Item(ItemBase):
     dish_id: int
 
     class Config:
