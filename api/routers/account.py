0eaa4ff9169c247996bfbc8e554883c265e5e231
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-07-29 18:37:46 -0400

    feat: updated functions in routers\account.py

--- api/routers/account.py
+++ api/routers/account.py
@@ -9,22 +9,27 @@ router = APIRouter(
     prefix="/account"
 )
 
+
 @router.post("/", response_model=schema.Account, status_code=status.HTTP_201_CREATED)
 def create(request: schema.AccountCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
+
 
 @router.get("/", response_model=list[schema.Account])
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
-@router.get("/{item_id}", response_model=schema.Account)
-def read_one(item_id: int, db: Session = Depends(get_db)):
-    return controller.read_one(db, item_id=item_id)
 
-@router.put("/{item_id}", response_model=schema.Account)
-def update(item_id: int, request: schema.AccountUpdate, db: Session = Depends(get_db)):
-    return controller.update(db=db, request=request, item_id=item_id)
+@router.get("/{account_id}", response_model=schema.Account)
+def read_one(account_id: int, db: Session = Depends(get_db)):
+    return controller.read_one(db, account_id=account_id)
 
-@router.delete("/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete(item_id: int, db: Session = Depends(get_db)):
-    return controller.delete(db=db, item_id=item_id)
+
+@router.put("/{account_id}", response_model=schema.Account)
+def update(account_id: int, request: schema.AccountUpdate, db: Session = Depends(get_db)):
+    return controller.update(db=db, request=request, account_id=account_id)
+
+
+@router.delete("/{account_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(account_id: int, db: Session = Depends(get_db)):
+    return controller.delete(db=db, account_id=account_id)
