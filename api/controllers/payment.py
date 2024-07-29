67358488b1ae141b279fa954179de735160dab92
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-07-29 18:52:11 -0400

    feat: updated functions in routers\payment.py

--- api/controllers/payment.py
+++ api/controllers/payment.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException, status, Response, Depends
 from ..models import payment as model
 from sqlalchemy.exc import SQLAlchemyError
 
+
 def create(db: Session, request):
     new_item = model.Payment(
         card_number=request.card_number,
@@ -30,9 +31,9 @@ def read_all(db: Session):
     return result
 
 
-def read_one(db: Session, item_id):
+def read_one(db: Session, card_number):
     try:
-        item = db.query(model.Payment).filter(model.Payment.id == item_id).first()
+        item = db.query(model.Payment).filter(model.Payment.card_number == card_number).first()
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -41,9 +42,9 @@ def read_one(db: Session, item_id):
     return item
 
 
-def update(db: Session, item_id, request):
+def update(db: Session, card_number, request):
     try:
-        item = db.query(model.Payment).filter(model.Payment.id == item_id)
+        item = db.query(model.Payment).filter(model.Payment.card_number == card_number)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         update_data = request.dict(exclude_unset=True)
@@ -55,9 +56,9 @@ def update(db: Session, item_id, request):
     return item.first()
 
 
-def delete(db: Session, item_id):
+def delete(db: Session, card_number):
     try:
-        item = db.query(model.Payment).filter(model.Payment.id == item_id)
+        item = db.query(model.Payment).filter(model.Payment.card_number == card_number)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         item.delete(synchronize_session=False)
