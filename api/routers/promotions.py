8d95a542e74bb40853b3b0c3a8c2f57efd1fd021
kspark12 <kspark12@uncc.edu>
2024-07-29 18:55:55 -0400

    feat: reformat of promotions,py

--- api/routers/promotions.py
+++ api/routers/promotions.py
@@ -9,21 +9,26 @@ router = APIRouter(
     prefix="/promotions"
 )
 
+
 @router.post("/", response_model=schema.Promotions, status_code=status.HTTP_201_CREATED)
 def create(request: schema.PromotionsCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
+
 
 @router.get("/", response_model=list[schema.Promotions])
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
+
 @router.get("/{item_id}", response_model=schema.Promotions)
 def read_one(item_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db, item_id=item_id)
 
+
 @router.put("/{item_id}", response_model=schema.Promotions)
 def update(item_id: int, request: schema.PromotionsUpdate, db: Session = Depends(get_db)):
     return controller.update(db=db, request=request, item_id=item_id)
+
 
 @router.delete("/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete(item_id: int, db: Session = Depends(get_db)):
