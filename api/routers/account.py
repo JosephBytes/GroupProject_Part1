45c66a3dc588654b921b5262bbe67472cb14d573
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-08-03 19:00:32 -0400

    feat: try/catch blocks added to routers\account.py

--- api/routers/account.py
+++ api/routers/account.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import FastAPI, HTTPException, APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from ..controllers import account as controller
 from ..schemas.account import Account, AccountCreate, AccountUpdate
@@ -12,25 +12,85 @@ router = APIRouter(
 
 @router.post("/", response_model=Account, status_code=status.HTTP_201_CREATED)
 def create(request: AccountCreate, db: Session = Depends(get_db)):
-    return controller.create(db=db, request=request)
+    try:
+        new_account = controller.create(db=db, request=request)
+        if new_account is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return new_account
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
 @router.get("/", response_model=list[Account])
 def read_all(db: Session = Depends(get_db)):
-    return controller.read_all(db)
+    try:
+        all_accounts = controller.read_all(db)
+        if all_accounts is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return all_accounts
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
 @router.get("/{account_id}", response_model=Account)
 def read_one(account_id: int, db: Session = Depends(get_db)):
-    return controller.read_one(db, account_id=account_id)
+    try:
+        acct = controller.read_one(db, account_id=account_id)
+        if acct is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return acct
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
 @router.put("/{account_id}", response_model=Account)
 def update(account_id: int, request: AccountUpdate, db: Session = Depends(get_db)):
-    return controller.update(db=db, request=request, account_id=account_id)
+    try:
+        updated = controller.update(db=db, request=request, account_id=account_id)
+        if updated is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return updated
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
 @router.delete("/{account_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete(account_id: int, db: Session = Depends(get_db)):
-    return controller.delete(db=db, account_id=account_id)
+    try:
+        deleted = controller.delete(db=db, account_id=account_id)
+        if deleted is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return deleted
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
