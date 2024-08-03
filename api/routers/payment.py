911f0a5871b37e4ee3f341a4efcdc80a0637a0db
Joseph <159864581+JosephBytes@users.noreply.github.com>
2024-08-03 18:49:40 -0400

    feat: try/catch blocks added to routers\payment.py

--- api/routers/payment.py
+++ api/routers/payment.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, FastAPI, status, Response
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from ..controllers import payment as controller
 from ..schemas import payment as schema
@@ -12,24 +12,84 @@ router = APIRouter(
 
 @router.post("/", response_model=schema.Payment)
 def create(request: schema.PaymentCreate, db: Session = Depends(get_db)):
-    return controller.create(db=db, request=request)
+    try:
+        new_payment = controller.create(db=db, request=request)
+        if new_payment is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return new_payment
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
 @router.get("/", response_model=list[schema.Payment])
 def read_all(db: Session = Depends(get_db)):
-    return controller.read_all(db)
+    try:
+        all_payments = controller.read_all(db)
+        if all_payments is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return all_payments
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
-@router.get("/{card_number}", response_model=schema.Payment)
+@router.get("/payment/{card_number}", response_model=schema.Payment)
 def read_one(card_number: int, db: Session = Depends(get_db)):
-    return controller.read_one(db, card_number=card_number)
+    try:
+        pmt = controller.read_one(db, card_number=card_number)
+        if pmt is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return pmt
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
-@router.put("/{card_number}", response_model=schema.Payment)
+@router.put("/payment/{card_number}", response_model=schema.Payment)
 def update(card_number: int, request: schema.PaymentUpdate, db: Session = Depends(get_db)):
-    return controller.update(db=db, request=request, card_number=card_number)
+    try:
+        updated = controller.update(db=db, request=request, card_number=card_number)
+        if updated is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return updated
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
 
 
-@router.delete("/{card_number}")
+@router.delete("/payment/{card_number}")
 def delete(card_number: int, db: Session = Depends(get_db)):
-    return controller.delete(db=db, card_number=card_number)
+    try:
+        deleted = controller.delete(db=db, card_number=card_number)
+        if deleted is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Payment not found"
+            )
+        return deleted
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
